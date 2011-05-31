aabbbabaefc57dd4c1f0ff9a7e38c5a851e66ea4
Michael Hohl <michael@hohl.co.at>
2011-05-31 20:42:58 +0200

    Fixed problem with PermissionHandler.

--- src/at/co/hohl/Announcer/AnnouncerPermissions.java
+++ src/at/co/hohl/Announcer/AnnouncerPermissions.java
@@ -17,13 +17,13 @@ public final class AnnouncerPermissions {
     /** The permission needed for receiving announcements this plugin. */
     public static final Permission RECEIVER = new Permission("announcer.receiver", false);
     /** The permission needed to add new announcements. */
-    public static final Permission ADD = new Permission("announcer.add");
+    public static final Permission ADD = new Permission("announcer.add", true);
     /** The permission needed to delete existing announcements. */
-    public static final Permission DELETE = new Permission("announcer.delete");
+    public static final Permission DELETE = new Permission("announcer.delete", true);
     /** The permission needed to broadcast existing announcements. */
-    public static final Permission BROADCAST = new Permission("announcer.broadcast");
+    public static final Permission BROADCAST = new Permission("announcer.broadcast", true);
     /** The permission needed for moderating this plugin. */
-    public static final Permission MODERATOR = new Permission("announcer.moderate");
+    public static final Permission MODERATOR = new Permission("announcer.moderate", true);
     /** The permission needed for moderating this plugin. */
-    public static final Permission ADMINISTRATOR = new Permission("announcer.admin");
+    public static final Permission ADMINISTRATOR = new Permission("announcer.admin", true);
 }
